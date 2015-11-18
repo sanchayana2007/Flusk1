154c86ee367888071d34842867979d3588388ab8
sanchayana2007 <sanchayana2007@gmail.com>
2015-11-18 20:47:40 +0530

    first simple Flusk basics

--- Hello-world.py
+++ Hello-world.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
+#pass the Module name to the Flask
 app = Flask(__name__)
+
+guesses = ['Python','perl','C++']
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+
+'''
+@app.route('/guess/<int:id>')
+def guess(id):
+    return render_template('guess.html',guess=guesses[id])
+'''
+@app.route('/question/<int:id>')
+def question(id):
+    return render_template('guess.html',question=guesses[id])
+
 
 if __name__ == '__main__':
-    app.run()
+    #This 0.0.0.0 will be connected to any machine can ping  your computer
+    app.run(host='0.0.0.0',port=5000,debug=True)
