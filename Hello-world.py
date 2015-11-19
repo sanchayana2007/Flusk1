8c34013e36f35b8c6ff45cb6af4276831b5715be
sanchayana2007 <sanchayana2007@gmail.com>
2015-11-19 18:53:29 +0530

    t5t
    e3e3

--- Hello-world.py
+++ Hello-world.py
@@ -1,24 +1,46 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
+from flask_wtf import Form
+from wtforms import RadioField,SubmitField
 #pass the Module name to the Flask
 app = Flask(__name__)
+app.config['SECRET_KEY']='secret!'
+
 
 guesses = ['Python','perl','C++']
+questions = ['Does it6 compile','Can we run that on VM']
+
+#Same HTML form template conveterd to class wtfform secured version
+class YesNoVersion(Form):
+    answer = RadioField('Yor answer',choices=[('yes','Yes'),('no','No')])
+    submit = SubmitField('Submit')
+
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-
-
-'''
 @app.route('/guess/<int:id>')
 def guess(id):
     return render_template('guess.html',guess=guesses[id])
-'''
-@app.route('/question/<int:id>')
+#this is invoked by the index and it has a form
+#this form will be updated and POSTED
+# The answer field of the form will be found
+@app.route('/question/<int:id>',methods= ['GET','POST'])
 def question(id):
-    return render_template('guess.html',question=guesses[id])
+    if request.method == 'POST':
+        if request.form['answer'] == "yes":
+            return redirect(url_for('question',id=id+1))
+    return render_template('question.html',question=questions[id])
+# Does the same work
+#It Dosent need a template it will render the Wtf form described in class
+@app.route('/question_wtf/<int:id>',methods= ['GET','POST'])
+def question_wtf(id):
+    form = YesNoVersion()
+    if form.validate_on_submit():
+        if form.answer.data == 'yes':
+            return redirect(url_for('question',id=id+1))
+    return render_template('question_wtf.html',question=questions[id],form=form)
+
 
 
 if __name__ == '__main__':
